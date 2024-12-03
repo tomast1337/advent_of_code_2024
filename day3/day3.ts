dccc99e1e1afd30958769b7ef57aca85309ff2b0
tomast1337 <vycasnicolas@gmail.com>
2024-12-03 14:16:30 -0300

    Implement day 3 solution with tests for part 1 and part 2

---
+++ day3/day3.ts
@@ -0,0 +1,43 @@
+export const part1 = (input: string): number => {
+  const regex = /mul\((\d+),(\d+)\)/g;
+  let match;
+  let sum = 0;
+
+  while ((match = regex.exec(input)) !== null) {
+    const x = parseInt(match[1], 10);
+    const y = parseInt(match[2], 10);
+    sum += x * y;
+  }
+
+  return sum;
+};
+
+export const part2 = (input: string): number => {
+  const mulRegex = /mul\((\d+),(\d+)\)/g;
+  const doRegex = /do\(\)/g;
+  const dontRegex = /don't\(\)/g;
+  let match;
+  let sum = 0;
+  let enabled = true;
+
+  let lastIndex = 0;
+  while ((match = mulRegex.exec(input)) !== null) {
+    // Check for do() and don't() instructions between the last match and the current match
+    const segment = input.slice(lastIndex, match.index);
+    if (doRegex.test(segment)) {
+      enabled = true;
+    } else if (dontRegex.test(segment)) {
+      enabled = false;
+    }
+
+    if (enabled) {
+      const x = parseInt(match[1], 10);
+      const y = parseInt(match[2], 10);
+      sum += x * y;
+    }
+
+    lastIndex = match.index + match[0].length;
+  }
+
+  return sum;
+};
