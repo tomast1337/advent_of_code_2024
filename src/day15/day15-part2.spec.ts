9e72df4d2a54501f93a820c8f391fd506d49bc0a
tomast1337 <vycasnicolas@gmail.com>
2024-12-16 18:56:58 -0300

    Add day15-part2.spec.ts with initial tests for part 1 functionality

---
+++ src/day15/day15-part2.spec.ts
@@ -0,0 +1,70 @@
+import fs from 'fs';
+import { part1 } from './day15-part2';
+
+describe('day15 part 2', () => {
+  let data: string;
+  beforeEach(() => {
+    data = fs.readFileSync('./src/day15/day15.txt', 'utf8');
+  });
+
+  it('should solve part 1 with small test data', () => {
+    expect(
+      part1(`#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^`),
+    );
+  });
+  /*
+  it('should solve part 1 with small test data', () => {
+    expect(
+      part1(`########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<`),
+    );
+  });
+  it('should solve part 1 with test data', () => {
+    expect(
+      part1(
+        `##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^`,
+      ),
+    ).toBe(9021);
+  });
+
+  it('should solve part 1', () => {
+    const result = part1(data);
+    console.log(`Result part1: ${result}`);
+  });*/
+});
